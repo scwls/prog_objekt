9f347fca0fdf80cd41fda4f7b3dcded2a9c69b0e
scwls <sashaleiko83@gmail.com>
2026-03-05 14:02:19 +0100

    zad5(lab2)

--- lab/lab1/src/Main.java
+++ lab/lab1/src/Main.java
@@ -1,39 +1,25 @@
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        Point p = new Point(5.4f, 3.8f);
-        System.out.println("RES: " + p);
-        System.out.println(p.toSVG());
-        p.translate(1.0f, 2.0f);
-        System.out.println("RES: " + p);
-        Point p2 = p.translated(2.0f, 1.0f);
-        System.out.println("RES: " + p2);
-        Point pk1 =new Point(2.0f, 3.0f);
-        Point pk2=new Point(4.0f, 8.0f);
-        Segment s = new Segment(pk1, pk2);
-        double len = s.length();
-        System.out.printf("Odleglosc=%.2f%n", len);
-        Segment[] ss=new Segment[5];
-        Random rand=new Random();
-        System.out.println("!!!!!!!!!!!!!"+s+"!!!!!!!!!");
-        pk1.setX(999.0f); //sprawdzamy
-        System.out.println("!!!!!!!!!!!!!"+s+"!!!!!!!!!");
-        System.out.println(s.toString1());
-        for(int i=0; i<5; i++){
-            Point randP1=new Point(rand.nextInt(10), rand.nextInt(10));
-            Point randP2=new Point(rand.nextInt(10), rand.nextInt(10));
-            ss[i]=new Segment(randP1, randP2);
-        }
-        Segment winner=Segment.longestSeg(ss);
-        System.out.println(winner);
-        Point[] trojkat=new Point[3];
-        trojkat[0]=new Point(0.0f, 0.0f);
-        trojkat[1]=new Point(0.0f, 5.0f);
-        trojkat[2]=new Point(5.0f, 0.0f);
-        Polygon pol_trojkat=new Polygon(trojkat);
-        Polygon temp=new Polygon(pol_trojkat);
-        trojkat[2]=new Point(3, 7);
-        System.out.println(temp.toSVG());
+        Point[] points=new Point[3];
+        points[0]=new Point(0.0f, 0.0f);
+        points[1]=new Point(0.0f, 5.0f);
+        points[2]=new Point(5.0f, 0.0f);
+        Polygon polygon=new Polygon(points);
+        Point[] points2 = new Point[3];
+        points2[0] = new Point(1.3F, 4.6F);
+        points2[1] = new Point(8.5F, 12.4F);
+        points2[2] = new Point(15.4F, 74.2F);
+        Polygon polygon2= new Polygon(points2);
+        Polygon polygon3 = new Polygon(new Point[]{
+                new Point(4.3F, 7.6F),
+                new Point(2.3F, 8.6F),
+                new Point(5.3F, 10.6F)
+        });
+        SvgScene svg=new SvgScene();
+        svg.addPolygon(polygon);
+        svg.addPolygon(polygon2);
+        svg.addPolygon(polygon3);
+        svg.addPolygon(polygon3);
     }
 }

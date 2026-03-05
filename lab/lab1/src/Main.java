cda1b6b71d264637ddb931db1a9c78f507a22aa0
scwls <sashaleiko83@gmail.com>
2026-03-05 14:35:48 +0100

    zad6(lab2)

--- lab/lab1/src/Main.java
+++ lab/lab1/src/Main.java
@@ -3,23 +3,24 @@ public class Main {
     public static void main(String[] args) {
         Point[] points=new Point[3];
         points[0]=new Point(0.0f, 0.0f);
-        points[1]=new Point(0.0f, 5.0f);
-        points[2]=new Point(5.0f, 0.0f);
+        points[1]=new Point(0.0f, 100.0f);
+        points[2]=new Point(100.0f, 0.0f);
         Polygon polygon=new Polygon(points);
         Point[] points2 = new Point[3];
-        points2[0] = new Point(1.3F, 4.6F);
-        points2[1] = new Point(8.5F, 12.4F);
-        points2[2] = new Point(15.4F, 74.2F);
+        points2[0] = new Point(78.3F, 200.6F);
+        points2[1] = new Point(15.5F, 18.4F);
+        points2[2] = new Point(190.4F, 74.2F);
         Polygon polygon2= new Polygon(points2);
         Polygon polygon3 = new Polygon(new Point[]{
-                new Point(4.3F, 7.6F),
-                new Point(2.3F, 8.6F),
-                new Point(5.3F, 10.6F)
+                new Point(25.3F, 25.6F),
+                new Point(75.3F, 75.6F),
+                new Point(15.3F, 90.6F)
         });
         SvgScene svg=new SvgScene();
         svg.addPolygon(polygon);
         svg.addPolygon(polygon2);
         svg.addPolygon(polygon3);
-        svg.addPolygon(polygon3);
+        //svg.addPolygon(polygon3);
+        System.out.println(svg.toSvg());
     }
 }
--- lab/lab1/src/SvgScene.java
+++ lab/lab1/src/SvgScene.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class SvgScene {
     private Polygon[] polygons=new Polygon[3];
     private int index=0;
@@ -8,4 +10,15 @@ public class SvgScene {
             index=0;
         }
     }
+    public String toSvg(){
+        String res="";
+
+        for(int i=0; i<polygons.length; i++){
+            if(polygons[i]!=null){
+                res+=polygons[i].toSVG()+"\n";
+            }
+        }
+        return String.format(Locale.ENGLISH, "<svg height=\"260\" width=\"500\" xmlns=\"http://www.w3.org/2000/svg\">\n%s</svg>", res);
+    }
+
 }

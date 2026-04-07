ead69a34bdf63042be98a96c0206acc0884d7bf0
scwls <sashaleiko83@gmail.com>
2026-04-07 18:01:38 +0200

    zad2-3(lab5)

--- newpr/genealogy/src/Main.java
+++ newpr/genealogy/src/Main.java
@@ -4,8 +4,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        List<Person> people= new ArrayList<>();
+        List<Person> people = new ArrayList<>();
         people.add(new Person("Mike","Tyson", LocalDate.of(1996, 6, 18)));
-        people.add(new Person("Muhammad", "Ali", LocalDate.of(1942, 1, 17)));
+        people.add(new Person("Muhammad", "Ali", LocalDate.of(2012, 1, 17)));
+        people.add(new Person("Тike","Tyson", LocalDate.of(2008, 6, 18)));
+        people.add(new Person("Max", "Ali", LocalDate.of(2007, 1, 17)));
+        Person parent = people.get(0);
+        Person child = people.get(1);
+        Person child1 = people.get(2);
+        Person child2 = people.get(3);
+        parent.adopt(child);
+        parent.adopt(child1);
+        parent.adopt(child2);
+        System.out.println(parent.adopt(parent));
+        System.out.println(people);
+        System.out.println(parent.getYoungestChild());
     }
 }
--- newpr/genealogy/src/Person.java
+++ newpr/genealogy/src/Person.java
@@ -1,9 +1,13 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Person {
-    private String firstname;
-    private String lastname;
-    private LocalDate birthday;
+    private final String firstname;
+    private final String lastname;
+    private final LocalDate birthday;
+    private final Set<Person> children = new HashSet<>();
 
     public Person(String firstname, String lastname, LocalDate birthday) {
         this.birthday=birthday;
@@ -11,5 +15,32 @@ public class Person {
         this.lastname=lastname;
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", birthday=" + birthday +
+                ", children=" + children +
+                '}';
+    }
 
+    public Person getYoungestChild(){
+        if(this.children.isEmpty()){
+            return null;
+        }
+        Person youngest=children.iterator().next();
+        for(Person person: children){
+            if (youngest.birthday.compareTo(person.birthday)>0){
+                youngest=person;
+            }
+        }
+        return youngest;
+    }
+
+
+    public boolean adopt(Person child){
+        if(child==this)return false;
+        return children.add(child);
+    }
 }

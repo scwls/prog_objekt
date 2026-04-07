27f9fafd8f75f94e6e9c43b8a45a1b9f10ff47e7
scwls <sashaleiko83@gmail.com>
2026-04-07 21:00:37 +0200

    zad7(lab5)

--- newpr/genealogy/src/Family.java
+++ newpr/genealogy/src/Family.java
@@ -2,8 +2,10 @@ import java.util.HashMap;
 import java.util.Map;
 public class Family {
     private final Map<String, Person> people = new HashMap<>();
-    public void add(Person person){
-        people.put(person.name(), person);
+    public void add(Person... people){
+        for(Person person: people){
+            this.people.put(person.name(), person);
+        }
     }
     public Person get(String key){
         return people.get(key);

38906453b279fbcf641a506719fdd9efd11b6ad8
scwls <sashaleiko83@gmail.com>
2026-04-09 11:23:53 +0200

    zad1(lab6)

--- newpr/genealogy/src/Main.java
+++ newpr/genealogy/src/Main.java
@@ -4,28 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        List<Person> people=new ArrayList<>();
-        people.add(new Person("Mike", "Tyson", LocalDate.of(1966, 5, 18)));
-        people.add(new Person("Daniło", "Korzemiakin", LocalDate.of(2015, 5, 12)));
-        people.add(new Person("Muhammad", "Ali", LocalDate.of(1944, 1, 2)));
-        people.add(new Person("Mike", "Tyson", LocalDate.of(2000, 5, 18)));
-        Person parent = people.get(0);
-        Person child = people.get(1);
-        System.out.println(parent.adopt(child));
-        System.out.println(parent.adopt(people.get(2)));
-        System.out.println(parent.getYoungestChild());
-
-        System.out.println(parent.getChildren());
-
-        Family family = new Family();
-//        for(Person person: people) {
-//            family.add(person);
-//        }
-        family.add(people.get(0));
-        family.add(people.get(1), people.get(2),people.get(3));
-
-        List<Person> mike = family.get("Mike Tyson");
-        System.out.println(mike);
-
+        Person person = Person.fromCsvLine("Marek Kowalski,15.05.1899,15.05.1999,,");
+        System.out.println(person);
     }
 }
--- newpr/genealogy/src/Person.java
+++ newpr/genealogy/src/Person.java
@@ -1,62 +1,86 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.HashSet;
-import java.util.Set;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Person implements Comparable<Person> {
-    private final String firstname;
-    private final String lastname;
+    private final String  firstName;
+    private final String lastName;
     private final LocalDate birthday;
+    private final LocalDate death;
+
     private final Set<Person> children = new HashSet<>();
 
-    public Person(String firstname, String lastname, LocalDate birthday) {
-        this.birthday=birthday;
-        this.firstname=firstname;
-        this.lastname=lastname;
+    public Person(String firstName, String lastName, LocalDate birthday, LocalDate death) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.death = death;
     }
 
-    @Override
-    public int compareTo(Person other){
-        return this.birthday.compareTo(other.birthday);
+    public Person(String firstName, String lastName, LocalDate birthday) {
+        this(firstName, lastName, birthday, null);
     }
 
-    public List<Person> getChildren(){
-        List<Person> result =new ArrayList<>();
-        result.addAll(children);
-        result.sort(Person::compareTo);
-        return result;
+    public static Person fromCsvLine(String line){
+        String[] columns = line.split(",", -1);
+        String fullname= columns[0];
+        String[] name = fullname.split(" ");
+        String fname = name[0];
+        String lname= name[1];
+        String birth=columns[1];
+        String death=columns[2];
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.y");
+        LocalDate birthdate = LocalDate.parse(birth, formatter);
+        LocalDate deathdate = null;
+        if(!death.isEmpty()){
+            deathdate = LocalDate.parse(death, formatter);
+        }
+        return new Person(fname, lname, birthdate, deathdate);
     }
 
     @Override
     public String toString() {
         return "Person{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
                 ", birthday=" + birthday +
+                ", death=" + death +
                 ", children=" + children +
                 '}';
     }
 
-    public Person getYoungestChild(){
-        if(this.children.isEmpty()){
+
+    public boolean adopt(Person child){
+        if (child == this) return false;
+        return children.add(child);
+    }
+
+
+    public Person getYoungestChild() {
+        if (this.children.isEmpty()) {
             return null;
         }
-        Person youngest=children.iterator().next();
-        for(Person person: children){
-            if (person.compareTo(youngest)>0){
-                youngest=person;
-            }
+        Person youngest = children.iterator().next();
+        for(Person person: children) {
+            if(youngest.compareTo(person)>0)
+                youngest = person;
         }
         return youngest;
     }
-    public String name(){
-        return String.format("%s, %s", firstname, lastname);
+
+    public List<Person> getChildren() {
+        List<Person> result = new ArrayList<>();
+        result.addAll(children);
+        result.sort(Person::compareTo);
+        return result;
     }
 
+    public String name() {
+        return String.format("%s %s",firstName, lastName);
+    }
 
-    public boolean adopt(Person child){
-        if(child==this)return false;
-        return children.add(child);
+    @Override
+    public int compareTo(Person other) {
+        return this.birthday.compareTo(other.birthday);
     }
 }

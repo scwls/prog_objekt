5144d1d4d2cf3598040e386029cd9ed44b63b804
scwls <sashaleiko83@gmail.com>
2026-06-11 10:51:29 +0200

    ----

--- chat/src/Main.java
+++ chat/src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Client client = new Client("127.0.0.1",3000);
         MainWindow mainWindow = new MainWindow();
         mainWindow.setVisible(true);
     }
